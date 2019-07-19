4a9154a44f1cc0c641d50648a4498ad6af9ee84a
Steve Frenzel <stevefrenzel@gmx.de>
2019-07-19 16:01:41 +0200

    Create MartianSol.js

---
+++ src/components/MartianSol.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class MartianSol extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <input type="number" name="martian_sol" />
+            </React.Fragment>
+        )
+    }
+}
