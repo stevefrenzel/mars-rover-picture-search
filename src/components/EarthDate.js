10355fa418f8f7e12f15a3aad26be58b2b4bbd05
Steve Frenzel <stevefrenzel@gmx.de>
2019-07-19 16:01:30 +0200

    Create EarthDate.js

---
+++ src/components/EarthDate.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class EarthDate extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <input type="date" name="earth_date" />
+            </React.Fragment>
+        )
+    }
+}
