aabaa68a5b72368c7af4d4ac9e56fd023ad0fb7b
Steve Frenzel <stevefrenzel@gmx.de>
2019-07-20 11:00:50 +0200

    Update MarsRover.js

--- src/components/MarsRover.js
+++ src/components/MarsRover.js
@@ -6,16 +6,22 @@ export default class MarsRover extends React.Component {
             <React.Fragment>
                 <div className='component' id='mars_rover_component'>
                     <h1>MarsRover.js</h1>
-                    <p><label htmlFor="rover_select">Choose a Mars rover:</label></p>
+                    <p>Choose a Mars rover:</p>
+                
+                    <div className='radio_button'>
+                        <input type="radio" id="select_curiosity" name="rover_select" value="curiosity" />
+                        <label htmlFor="curiosity">Curiosity</label>
+                    </div>
 
-                    <input type="radio" id="select_curiosity" name="rover_select" value="curiosity" />
-                    <label htmlFor="select_curiosity">Curiosity</label>
+                    <div className='radio_button'>
+                        <input type="radio" id="select_opportunity" name="rover_select" value="opportunity" />
+                        <label htmlFor="opportunity">Opportunity</label>
+                    </div>
 
-                    <input type="radio" id="select_opportunity" name="rover_select" value="opportunity" />
-                    <label htmlFor="select_opportunity">Opportunity</label>
-
-                    <input type="radio" id="select_spirit" name="rover_select" value="spirit" />
-                    <label htmlFor="select_spirit">Spirit</label>
+                    <div className='radio_button'>
+                        <input type="radio" id="select_spirit" name="rover_select" value="spirit" />
+                        <label htmlFor="spirit">Spirit</label>
+                    </div>
                 </div>
             </React.Fragment>
         )
