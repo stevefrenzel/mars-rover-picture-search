a0989118de313d102bfd09469d6174a29e49b2bf
Steve Frenzel <stevefrenzel@gmx.de>
2019-07-20 09:53:22 +0200

    Update OpportunityAndSpirit.js

--- src/components/OpportunityAndSpirit.js
+++ src/components/OpportunityAndSpirit.js
@@ -4,14 +4,25 @@ export default class OpportunityAndSpirit extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <label htmlFor="camera_select">Choose a camera: </label>
-                <select id="camera_select">
-                    <option value="fhaz">Front Hazard Avoidance Camera</option>
-                    <option value="rhaz">Rear Hazard Avoidance Camera</option>
-                    <option value="navcam">Navigation Camera</option>
-                    <option value="pancam">Panoramic Camera</option>
-                    <option value="minites">Miniature Thermal Emission Spectrometer (Mini-TES)</option>
-                </select>
+                <div className='component' id='opportunity_spirit_component'>
+                    <h1>OpportunityAndSpirit.js</h1>
+                    <p><label htmlFor="camera_select">Choose a camera: </label></p>
+
+                    <input type="radio" id="select_camera" name="camera_select" value="fhaz" />
+                    <label htmlFor="select_camera">Front Hazard Avoidance Camera</label>
+
+                    <input type="radio" id="select_camera" name="camera_select" value="rhaz" />
+                    <label htmlFor="select_camera">Rear Hazard Avoidance Camera</label>
+
+                    <input type="radio" id="select_camera" name="camera_select" value="navcam" />
+                    <label htmlFor="select_camera">Navigation Camera</label>
+
+                    <input type="radio" id="select_camera" name="camera_select" value="pancam" />
+                    <label htmlFor="select_camera">Panoramic Camera</label>
+
+                    <input type="radio" id="select_camera" name="camera_select" value="minites" />
+                    <label htmlFor="select_camera">Miniature Thermal Emission Spectrometer (Mini-TES</label>
+                </div>
             </React.Fragment>
         )
     }
