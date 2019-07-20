0123bf2d256a0c4885246c325bb9fa211eb817d1
Steve Frenzel <stevefrenzel@gmx.de>
2019-07-20 17:26:53 +0200

    Update MartianSol.js

--- src/components/MartianSol.js
+++ src/components/MartianSol.js
@@ -7,14 +7,9 @@ export default class MartianSol extends React.Component {
             <React.Fragment>
                 <div className='component' id='martian_sol_component'>
                     <h1>MartianSol.js</h1>
-                    <input onInput={this.props.selectMartianSol} id='martian_sol_input' type='number' name='martian_sol' />
+                    <input onInput={this.props.selectMartianSol} id='martian_sol_input' type='number' name='martian_sol' required />
                 </div>
             </React.Fragment>
         )
     }
 }
-
-// TO DO:
-
-// restrict input to numbers ONLY
-// if input empty set dateEntered to false
