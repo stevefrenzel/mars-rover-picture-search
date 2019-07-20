786e4a0dbac293379e5eae23cb9e6070619140cc
Steve Frenzel <stevefrenzel@gmx.de>
2019-07-20 11:40:14 +0200

    Update MartianSol.js

--- src/components/MartianSol.js
+++ src/components/MartianSol.js
@@ -1,14 +1,28 @@
 import React from 'react';
 
 export default class MartianSol extends React.Component {
+    state = {
+        martian_sol: null
+    }
+
+    selectDate = (e) => {
+        this.setState({
+            martian_sol: e.target.value,
+        });
+    }
+
     render() {
         return (
             <React.Fragment>
                 <div className='component' id='martian_sol_component'>
                     <h1>MartianSol.js</h1>
-                    <input id='martian_sol_input' type='number' name='martian_sol' />
+                    <input onInput={this.selectDate} id='martian_sol_input' type='number' name='martian_sol' />
                 </div>
             </React.Fragment>
         )
     }
 }
+
+// TO DO:
+
+// restrict input to numbers ONLY
