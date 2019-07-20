63522c3e310e5dd21417339d8c247f5afa31be77
Steve Frenzel <stevefrenzel@gmx.de>
2019-07-20 14:23:23 +0200

    Update EarthDate.js

--- src/components/EarthDate.js
+++ src/components/EarthDate.js
@@ -1,22 +1,13 @@
 import React from 'react';
 
 export default class EarthDate extends React.Component {
-    state = {
-        earth_date: null
-    }
-
-    selectDate = (e) => {
-        this.setState({
-            earth_date: e.target.value,
-        });
-    }
 
     render() {
         return (
             <React.Fragment>
                 <div className='component' id='earth_date_component'>
                     <h1>EarthDate.js</h1>
-                    <input onInput={this.selectDate} id='earth_date_input' type='date' name='earth_date' />
+                    <input onInput={this.props.selectEarthDate} id='earth_date_input' type='date' name='earth_date' />
                 </div>
             </React.Fragment>
         )
