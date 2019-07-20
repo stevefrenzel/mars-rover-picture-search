6c9f560449ecbc2c08194e2208a374da159d00fa
Steve Frenzel <stevefrenzel@gmx.de>
2019-07-20 09:53:08 +0200

    Update EarthDate.js

--- src/components/EarthDate.js
+++ src/components/EarthDate.js
@@ -4,7 +4,10 @@ export default class EarthDate extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <input type="date" name="earth_date" />
+                <div className='component' id='earth_date_component'>
+                    <h1>EarthDate.js</h1>
+                    <input type="date" name="earth_date" />
+                </div>
             </React.Fragment>
         )
     }
