33ab0b823a9725d61dfb4fb45eac6b80ecfcf465
Steve Frenzel <stevefrenzel@gmx.de>
2019-07-20 17:27:00 +0200

    Create SubmitButton.js

---
+++ src/components/SubmitButton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class SubmitButton extends React.Component {
+
+    render() {
+        return (
+            <React.Fragment>
+                <div className='component' id='submit_button_component'>
+                    <h1>SubmitButton.js</h1>
+                    <button>Get Photos</button>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
