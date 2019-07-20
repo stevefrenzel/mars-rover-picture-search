925fcf2de07a816bb71d3bb93b294a3e50a430c1
Steve Frenzel <stevefrenzel@gmx.de>
2019-07-20 17:27:02 +0200

    Update Timezone.js

--- src/components/Timezone.js
+++ src/components/Timezone.js
@@ -9,12 +9,12 @@ export default class Timezone extends React.Component {
                     <p>Choose a timezone:</p>
 
                     <div className='radio_button'>
-                        <input onClick={this.props.selectTimezone} type="radio" id="select_earth_date" name="timezone_select" value="earth_date" />
+                        <input onClick={this.props.selectTimezone} type="radio" id="select_earth_date" name="timezone_select" value="earth_date" required />
                         <label htmlFor="select_earth_date">Earth Date</label>
                     </div>
 
                     <div className='radio_button'>
-                        <input onClick={this.props.selectTimezone} type="radio" id="select_martian_sol" name="timezone_select" value="martian_sol" />
+                        <input onClick={this.props.selectTimezone} type="radio" id="select_martian_sol" name="timezone_select" value="martian_sol" required />
                         <label htmlFor="select_martian_sol">Martian Sol</label>
                     </div>
                 </div>
