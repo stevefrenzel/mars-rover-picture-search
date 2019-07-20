299d0540ea5e9282e9f016194894d8ebe53c877f
Steve Frenzel <stevefrenzel@gmx.de>
2019-07-20 09:54:08 +0200

    Update Title.js

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,10 +4,10 @@ export default class Title extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <element className='component' id='title_component'>
-                <h1>Mars Rover Picture Search</h1>
-                <p>Search for photos of Mars rover Curiousity, Opportunity and Spirit!</p>
-                </element>
+                <div className='component' id='title_component'>
+                    <h1>Mars Rover Picture Search</h1>
+                    <p>Search for photos of Mars rover Curiousity, Opportunity and Spirit!</p>
+                </div>
             </React.Fragment>
         );
     }
