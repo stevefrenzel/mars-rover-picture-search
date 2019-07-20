3f7d81ef37ffdf4d3e01af3e1487e1b2c0249290
Steve Frenzel <stevefrenzel@gmx.de>
2019-07-20 11:40:17 +0200

    Update OpportunityAndSpirit.js

--- src/components/OpportunityAndSpirit.js
+++ src/components/OpportunityAndSpirit.js
@@ -1,6 +1,16 @@
 import React from 'react';
 
 export default class OpportunityAndSpirit extends React.Component {
+    state = {
+        camera: null
+    }
+
+    selectCamera = (e) => {
+        this.setState({
+            camera: e.target.value,
+        });
+    }
+
     render() {
         return (
             <React.Fragment>
@@ -9,27 +19,27 @@ export default class OpportunityAndSpirit extends React.Component {
                     <p>Choose a camera:</p>
 
                     <div className='radio_button'>
-                        <input type="radio" id="fhaz_camera_opportunity_spirit" name="camera_select_opportunity_spirit" value="fhaz" />
+                        <input onClick={this.selectCamera} type="radio" id="fhaz_camera_opportunity_spirit" name="camera_select_opportunity_spirit" value="fhaz" />
                         <label htmlFor="fhaz_camera_opportunity_spirit">Front Hazard Avoidance Camera</label>
                     </div>
 
                     <div className='radio_button'>
-                        <input type="radio" id="rhaz_camera_opportunity_spirit" name="camera_select_opportunity_spirit" value="rhaz" />
+                        <input onClick={this.selectCamera} type="radio" id="rhaz_camera_opportunity_spirit" name="camera_select_opportunity_spirit" value="rhaz" />
                         <label htmlFor="rhaz_camera_opportunity_spirit">Rear Hazard Avoidance Camera</label>
                     </div>
 
                     <div className='radio_button'>
-                        <input type="radio" id="navcam_camera_opportunity_spirit" name="camera_select_opportunity_spirit" value="navcam" />
+                        <input onClick={this.selectCamera} type="radio" id="navcam_camera_opportunity_spirit" name="camera_select_opportunity_spirit" value="navcam" />
                         <label htmlFor="navcam_camera_opportunity_spirit">Navigation Camera</label>
                     </div>
 
                     <div className='radio_button'>
-                        <input type="radio" id="pancam_camera_opportunity_spirit" name="camera_select_opportunity_spirit" value="pancam" />
+                        <input onClick={this.selectCamera} type="radio" id="pancam_camera_opportunity_spirit" name="camera_select_opportunity_spirit" value="pancam" />
                         <label htmlFor="pancam_camera_opportunity_spirit">Panoramic Camera</label>
                     </div>
 
                     <div className='radio_button'>
-                        <input type="radio" id="minites_camera_opportunity_spirit" name="camera_select_opportunity_spirit" value="minites" />
+                        <input onClick={this.selectCamera} type="radio" id="minites_camera_opportunity_spirit" name="camera_select_opportunity_spirit" value="minites" />
                         <label htmlFor="minites_camera_opportunity_spirit">Miniature Thermal Emission Spectrometer (Mini-TES)</label>
                     </div>
                 </div>
