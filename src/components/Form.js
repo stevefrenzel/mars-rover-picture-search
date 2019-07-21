1d18591d8fe64b3c2c920577a8d138168b404c3f
Steve Frenzel <stevefrenzel@gmx.de>
2019-07-21 09:38:32 +0200

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -9,56 +9,90 @@ import SubmitButton from './SubmitButton';
 
 export default class Form extends React.Component {
     state = {
+
+        // DATA FOR API CALL
         rover: null,
         earth_date: null,
         martian_sol: null,
         camera: null,
         
+        // DETERMINING WHICH ROVER WAS SELECTED
+        curiositySelected: false,
+        opportunitySelected: false,
+        spiritSelected: false,
+
+        // DETERMINING WHICH TIMEZONE WAS SELECTED
+        // AND IF DATE HAS BEEN ENTERED
+
+        earthDateSelected: false,
         earthDateEntered: false,
+        martianSolSelected: false,
         martianSolEntered: false,
 
-        roverSelected: false,
-        earthDateSelected: false,
-        martianSolSelected: false,
-        cameraSelected: false,
-        curiositySelected: false,
-        opportunityOrSpiritSelected: false
+        cameraSelected: false
     };
 
     // GETTING DATA FROM CHILD COMPONENTS
 
     selectRover = (e) => {
-        this.setState({ rover: e.target.value, roverSelected: true });
+        if (e.target.value === 'curiosity') {
+            this.setState({ 
+                rover: e.target.value, 
+                curiositySelected: true
+             });
+        } else if (e.target.value === 'opportunity') {
+            this.setState({ 
+                rover: e.target.value, 
+                opportunitySelected: true
+            });
+        } else if (e.target.value === 'spirit') {
+            this.setState({ 
+                rover: e.target.value, 
+                spiritSelected: true
+             });
+        } 
+    }
+
+    selectCamera = (e) => {
+            this.setState({ 
+                camera: e.target.value, 
+                cameraSelected: true 
+            });
     }
 
     // how to only setState when radio button is checked?
     selectTimezone = (e) => {
         if (e.target.value === 'earth_date') {
-            this.setState({ timezone: e.target.value, earthDateSelected: true });
+            this.setState({ 
+                timezone: e.target.value, 
+                earthDateSelected: true 
+            });
         } else if (e.target.value === 'martian_sol'){
-            this.setState({ timezone: e.target.value, martianSolSelected: true });
+            this.setState({ 
+                timezone: e.target.value, 
+                martianSolSelected: true 
+            });
         }
     }
 
     selectEarthDate = (e) => {
-        this.setState({ earth_date: e.target.value, earthDateEntered: true });
+        this.setState({ 
+            earth_date: e.target.value, 
+            earthDateEntered: true 
+        });
     }
 
     selectMartianSol = (e) => {
         if (e.target.value === '') {
             this.setState({ dateEntered: false });
         } else {
-            this.setState({ martian_sol: e.target.value, martianSolEntered: true });
+            this.setState({ 
+                martian_sol: e.target.value, 
+                martianSolEntered: true 
+            });
         }        
     }
 
-    selectCamera = (e) => {
-        if (e.target.value === 'curiosity') {
-            this.setState({ camera: e.target.value, curiositySelected: true });
-        } else if (e.target.value === 'opportunity' || 'spirit') {
-            this.setState({ camera: e.target.value, opportunityOrSpiritSelected: true });
-        }
-    }
 
     render() {
 
@@ -72,31 +106,27 @@ export default class Form extends React.Component {
 
                         <MarsRover selectRover={this.selectRover}/>
 
+                        {/* CHOOSE CAMERA */}
+
+                        {this.state.curiositySelected ? <Curiosity selectCamera={this.selectCamera} /> : null}
+
+                        {this.state.opportunitySelected || this.state.spiritSelected ? <OpportunityAndSpirit selectCamera={this.selectCamera} /> : null}
+
                         {/* CHOOSE TIMEZONE */}
 
-                        {this.state.roverSelected ? <Timezone selectTimezone={this.selectTimezone} /> : null}
+                        <Timezone selectTimezone={this.selectTimezone} />
                                                 
                         {/* ENTER EARTH DATE OR MARTIAN SOL */}
 
                         {this.state.earthDateSelected ? <EarthDate selectEarthDate={this.selectEarthDate} /> : null}
                       
                         {this.state.martianSolSelected ? <MartianSol selectMartianSol={this.selectMartianSol} /> : null}
-                        
-                        {/* CHOOSE CAMERA */}
-
-                        <Curiosity selectCamera={this.selectCamera} />
-
-                        <OpportunityAndSpirit selectCamera={this.selectCamera} />
 
                         {
-                            (this.state.roverSelected && 
-                            this.state.timezoneSelected &&  
-                            this.state.cameraSelected) && 
-                            (this.state.earthDateEntered || this.state.martianSolEntered) ? 
-                            <SubmitButton /> : 
-                            null
-                        }
-                        
+                            ((this.state.curiositySelected || this.state.opportunitySelected || this.state.spiritSelected) && 
+                            this.state.timezoneSelected && 
+                            (this.state.earthDateEntered || this.state.martianSolEntered) && 
+                            this.state.cameraSelected) ? <SubmitButton /> : null}
                     </form>
                 </div>
             </React.Fragment>
@@ -106,7 +136,8 @@ export default class Form extends React.Component {
 
 // TO DO:
 
-// make all inputs required BEFORE submitting!
+// How to keep track of checked radio buttons?
+// (If certain buttons are unchecked, certain components need to disappear)
 
 // How to erase input from one date after switching to the other one?
 // (To avoid two inputs, which should make an invalid API call)
