616d3ee9a7cdc9ca3845d63fb8e4c610a28eeeb7
Steve Frenzel <stevefrenzel@gmx.de>
2019-07-28 10:36:37 +0200

    Update MartianSol.js

--- src/components/MartianSol.js
+++ src/components/MartianSol.js
@@ -7,7 +7,7 @@ export default class MartianSol extends React.Component {
             <React.Fragment>
                 <div className='component' id='martian_sol_component'>
                     <h2>Enter a number:</h2>
-                    <input onInput={this.props.selectMartianSol} id='martian_sol_input' type='number' name='martian_sol' required />
+                    <input onInput={this.props.selectMartianSol} id='martian_sol_input' type='number' name='martian_sol' required autoFocus/>
                 </div>
             </React.Fragment>
         )
