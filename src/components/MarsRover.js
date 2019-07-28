804183bad7c60a93da1ac6b29a0dd34b1fe5cfb3
Steve Frenzel <stevefrenzel@gmx.de>
2019-07-28 08:15:12 +0200

    Update MarsRover.js

--- src/components/MarsRover.js
+++ src/components/MarsRover.js
@@ -6,22 +6,23 @@ export default class MarsRover extends React.Component {
         return (
             <React.Fragment>
                 <div className='component' id='mars_rover_component'>
-                    <h1>MarsRover.js</h1>
-                    <p>Choose a Mars rover:</p>
-                
-                    <div className='radio_button'>
-                        <input onClick={this.props.selectRover} type="radio" id="select_curiosity" name="rover_select" value="curiosity" required/>
-                        <label htmlFor="curiosity">Curiosity</label>
-                    </div>
+                    <h2>Choose a Mars rover:</h2>
 
-                    <div className='radio_button'>
-                        <input onClick={this.props.selectRover} type="radio" id="select_opportunity" name="rover_select" value="opportunity" required />
-                        <label htmlFor="opportunity">Opportunity</label>
-                    </div>
+                    <div className="button-wrapper">
+                        <div className='radio_button'>
+                            <input onClick={this.props.selectRover} type="radio" id="select_curiosity" name="rover_select" value="curiosity" required/>
+                            <label htmlFor="curiosity">Curiosity</label>
+                        </div>
 
-                    <div className='radio_button'>
-                        <input onClick={this.props.selectRover} type="radio" id="select_spirit" name="rover_select" value="spirit" required />
-                        <label htmlFor="spirit">Spirit</label>
+                        <div className='radio_button'>
+                            <input onClick={this.props.selectRover} type="radio" id="select_opportunity" name="rover_select" value="opportunity" required />
+                            <label htmlFor="opportunity">Opportunity</label>
+                        </div>
+
+                        <div className='radio_button'>
+                            <input onClick={this.props.selectRover} type="radio" id="select_spirit" name="rover_select" value="spirit" required />
+                            <label htmlFor="spirit">Spirit</label>
+                        </div>
                     </div>
                 </div>
             </React.Fragment>
