13475b7ffbb3a82c29dc2725f92cfa22f2794fb8
Steve Frenzel <stevefrenzel@gmx.de>
2019-07-28 08:15:19 +0200

    Update Pictures.js

--- src/components/Pictures.js
+++ src/components/Pictures.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import { log } from 'util';
 
 export default class Pictures extends React.Component {
     render() {
-        console.log(this.props);
-        
         if (!this.props.photos) {
             return null;
         }
-
-        if (!this.props.photos.length) {
+        if (this.props.photos.length === 0) {
             return (
                 <React.Fragment>
                     <h1>NO PHOTOS AVAILABLE!</h1>
@@ -19,14 +15,6 @@ export default class Pictures extends React.Component {
         return (
             <React.Fragment>
                 <div className='component' id='pictures_component'>
-                    <h1>Pictures.js</h1>
-                    { (this.props.rover, this.props.time, this.props.camera && (
-                            <p>
-                                Mars rover: {this.props.rover} • Martian Sol:{' '}
-                                {this.props.time} • Camera: {this.props.camera}
-                            </p>
-                        ))
-                    }
                     {this.props.photos.photos.map(photo => (<img src={photo.img_src} alt="enter dynamic context" key={photo.id} />))}
                 </div>
             </React.Fragment>
